1f6d2aabb6e543be29f3725bf16b64537fc41cf3
Salik <iamsalikjamal@gmail.com>
2022-12-30 17:43:52 +0500

    Added features for voting on proposals

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useMemo } from 'react'
 import { useAddress, useContract, useNFTBalance, Web3Button } from "@thirdweb-dev/react"
+import { AddressZero } from '@ethersproject/constants'
 
 
 const App = () => {
@@ -40,6 +41,89 @@ const App = () => {
             }
         })
     }, [memberAddresses, memberTokenAmounts])
+
+    const handleProposalForm = async (e) => {
+        e.preventDefault()
+        e.stopPropagation()
+
+        // Disable button, to prevent double clicks
+        setIsVoting(true)
+
+        // Get votes from the form for values
+        const votes = proposals.map(proposal => {
+            const voteResult = {
+                proposalId: proposal.proposalId,
+                vote: 2 // Abstain by default
+            }
+            proposal.votes.foreach(vote => {
+                const elem = document.getElementById(`${proposal.proposalId}-${vote.type}`)
+                if(elem.checked) {
+                    voteResult.vote = vote.type
+                    return
+                }
+            })
+            return voteResult
+        })
+
+        // First we need to make sure user delegates their token to vote
+        try {
+            // We'll check if the wallet still needs to delegate their tokens before they can vote
+            const delegation = await token.getDelegation(address)
+            // If the delegation is the 0x0 address that means they have not delegated their governance tokens yet
+            if(delegation === AddressZero) {
+                // If they haven't delegated their tokens yet, we'll have them delegate them before voting
+                await token.delegateTo(address)
+            }
+
+            // Then we need to vote on the proposals
+            try {
+
+                await Promise.all(
+                    votes.map(async ({ proposalId, vote: _vote }) => {
+                        // Before voting we first need to check whether the proposal is open for voting
+                        // We first need to get the latest state of the proposal
+                        const proposal = await vote.get(proposalId)
+                        // Then we check if the proposal is open for voting (state === 1 means it is open)
+                        if(proposal.state === 1) return vote.vote(proposalId, _vote)
+
+                        // If the proposal is not open for voting we just return nothing, letting us continue
+                        return
+                    })
+                )
+
+                try {
+
+                    // If any of the propsals are ready to be executed we'll need to execute them
+                    // a proposal is ready to be executed if it is in state 4
+                    await Promise.all(votes.map(async ({ proposalId }) => {
+
+                        // We'll first get the latest state of the proposal again, since we may have just voted before
+                        const proposal = await vote.get(proposalId)
+
+                        // If the state is in state 4 (meaning that it is ready to be executed), we'll execute the proposal
+                        if(proposal.state === 4) return vote.execute(proposalId)
+
+                    }))
+
+                    setHasVoted(true)
+                    console.log('Successfully voted!')
+
+
+                } catch(err) {
+                    console.error(`Failed to excute a vote: ${err}`)
+                }
+
+            } catch(err) {
+                console.error(`Failed to vote: ${err}`)
+            }
+
+
+        } catch(err) {
+            console.error('Failed to delegate tokens')
+        } finally {
+            setIsVoting(false)
+        }
+    }
 
     // This useEffect grabs all the addresses of our members holding our NFT.
     useEffect(() => {
@@ -145,6 +229,30 @@ const App = () => {
                                 })}
                             </tbody>
                         </table>
+                    </div>
+                    <div>
+                        <h2>Active Proposals</h2>
+                        <form onSubmit={handleProposalForm}>
+                            {proposals.map(proposal => (
+                                <div key={proposal.proposalId} className='card'>
+                                    <h5>{proposal.description}</h5>
+                                    <div>
+                                        {proposal.votes.map(({ type, label }) => (
+                                            <div key={type}>
+                                                <input type='radio' id={`${proposal.proposalId}-${type}`} name={proposal.proposalId} value={type} defaultChecked={type === 2} />
+                                                <label htmlFor={`${proposal.proposalId}-${type}`}>{label}</label>
+                                            </div>
+                                        ))}
+                                    </div>
+                                </div>
+                            ))}
+                            <button disabled={isVoting || hasVoted} type='submit'>
+                                {isVoting ? 'Voting...' : hasVoted ? 'You already voted!' : 'Submit vote'}
+                            </button>
+                            {!hasVoted && (
+                                <small>This will trigger multiple transaction that you will need to sign.</small>
+                            )}
+                        </form>
                     </div>
                 </div>
             </div>
